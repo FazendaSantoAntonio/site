ce4a17289dda60027bd288c0caab56ed0c5923f8
Hebert <hebertdev82@gmail.com>
2024-02-05 14:19:57 -0300

    feat: history

--- src/app/quem-somos/page.jsx
+++ src/app/quem-somos/page.jsx
@@ -1,24 +1,100 @@
 export default function QuemSomos() {
-    return (
-        <div className="flex flex-col px-5 py-16  md:p-16 text-primary bg-light">
-            <h2 className="text-4xl font-bold flex justify-center items-center w-full py-10">Quem Somos</h2>
-            <p>
-                Bem-vindas (os) À Fazenda Santo Antônio!
-            </p><br />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati, modi nemo enim quos eaque libero repudiandae reprehenderit nulla incidunt consequatur ea quidem perferendis, unde nihil praesentium minima fuga! Enim?
-            </p><br />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati, modi nemo enim quos eaque libero repudiandae reprehenderit nulla incidunt consequatur ea quidem perferendis, unde nihil praesentium minima fuga! Enim?
-            </p><br />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati, modi nemo enim quos eaque libero repudiandae reprehenderit nulla incidunt consequatur ea quidem perferendis, unde nihil praesentium minima fuga! Enim?
-            </p><br />
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa obcaecati, modi nemo enim quos eaque libero repudiandae reprehenderit nulla incidunt consequatur ea quidem perferendis, unde nihil praesentium minima fuga! Enim?
-            </p><br />
-            
-
-        </div>
-    )
+  return (
+    <div className="flex flex-col px-5 py-16  md:p-16 text-primary bg-light">
+      <h2 className="text-4xl font-bold flex justify-center items-center w-full py-10">
+        Quem Somos
+      </h2>
+      <p>Veja o relato do Marcos, Produtor e Proprietário da Fazenda Santo Antônio:</p>
+      <br />
+      <p>
+        Quando eu era criança, meu avô tinha um laticínio no bairro Quilombo, em
+        Alagoa, laticínio antigo. O Prefeito da Cidade de Alagoa, em Minas
+        Gerais, está pensando em tombar como patrimônio histórico esse
+        latícinio. Meu pai tinha um colaborador chamado `Seu Chico`, que
+        trabalhava e fazia os queijos com ele, eu tinha por volta de 7 anos
+        nessa época. Eu, sempre apaixonado por queijo e procurando sempre
+        acompanhar meu pai, não podia ver uma massa de queijo que ficava doido
+        pra comer! Eu ia no laticínio todos os dias, depois da escola, estudava
+        na parte na tarde. Todas os dias que passava no laticínio, `Seu Chico`
+        falava pra mim: `você pode comer a massa do queijo, mas vai ter que
+        fazer também`. Fui ajudando e aprendendo com ele, desde pequeno.
+        <br />
+        <br />
+        Ele foi me ensinando a fazer, como enformar, dar ponto na massa, há
+        muitos anos atrás. Hoje tenho 40 anos, tem mais de 30 anos que iniciei a
+        produção de queijos. De lá pra cá, eu mexo com queijo, desde criança. A
+        história da minha família vem desde os avós mexendo com leite, e nesse
+        caminho acabei começando a a mexer com as vacas. Comprei uma bezerrinha,
+        meu sonho era ser dono de fazenda, mas não sabia que dava tanto
+        trabalho... Começou na brincadeira, trabalhava de ajudante de fazenda,
+        fui juntando o dinheiro que ganhava deste trabalho para comprar minha
+        primeira bezerra, depois comprei mais uma, viraram novilho, vaca, e
+        então comecei a mexer com leite, com poucas vacas, comecei a mexer com
+        queijo, bem pouquinho...
+        <br />
+        <br />
+        Fui crescendo devagar na produção...
+        <br />
+        <br />
+        Depois de muitos anos mexendo com leite, já tinha crescido um pouco,
+        mais vacas e adquiri uma estrutura melhor. Casei com a Patrícia, que é
+        minha esposa até hoje! Por conta de meu casamento que surgiu o nome da
+        fazenda e do queijo, porque o meu sogro chamava Antônio, uma pessoa
+        maravilhosa, muito bacana, um paizão pra mim, tinha muito luxo com a
+        família, muito amoroso, uma bondade de pessoa. Ele faleceu há 13 anos
+        aproximadamente, o nome da marca foi em homenagem a ele!
+        <br />
+        <br />
+        Surgiu a necessidade de pensar no rótulo e na marca do queijo, pois os
+        Queijos da Alagoa começaram a ficar conhecidos no Brasil e no Exterior.
+        A gente não tinha costume de usar rótulos ou marca em nosssos queijos, a
+        gente vendia tudo no saquinho mesmo, coisa simples, a gente punha num
+        saco de ração mesmo e mandava para o atravessador, pagava muito
+        pouquinho pra ele, quase não tinha lucro. Percebi alguns laticínios
+        criando rótulos na frente do meu e achei interessante, a gente precisava
+        criar uma marca do queijo da gente. Desde então, resolvi fazer o rótulo,
+        demorei uns 2 anos para finalizar essa etapa.
+        <br />
+        <br />
+        Pintou um Concurso em São Paulo chamado Prêmio Queijo Brasil, o primeiro
+        concurso que disputei, não tinha conhecimento com concurso, mas fui com
+        muita fé participar. Esse concurso aconteceu em 2018, tinha pouco
+        conhecimento mesmo, fiz três queijos e mandei, fiquei na expectativa,
+        aguardando pelo resultado. De repente, recebi a noticia de que havia
+        ganhado 3 medalhas de ouro nesse concurso. Fiquei extremamente feliz!!!
+        <br />
+        <br />
+        Passados 6 meses deste primeiro concurso, apareceu o proximo, que foi o
+        de Araxá, Prêmio Queijo Brasil. Mandei mais 3 queijos, acabei ganhando 2
+        medalhas de prata e uma bronze. Isso me deixou também muito feliz. Esse
+        foi em 2019. No mesmo ano, teve novamente o Queijo Brasil em SP, mandei
+        mais 3 queijos e ganhei mais 2 ouros e 1 prata! Assim a marca foi
+        ficando cada vez mais famosa.
+        <br />
+        <br />
+        De 2020 pra 2021 pintou o Concurso na França. Já tinham 2 produtores da
+        cidade que tinham Medalha de Ouro na França. O Osvaldinho `Queijo
+        Alagoa` e o Renato `Bela Vista`. Pensei comigo, se eu ganhasse seria
+        muito legal também, um premio internacional. Virei associado da
+        `Sertobras` e a Débora, que é Presidente e mora na França me auxiliou na
+        inscrição do concurso. Inscrevi o meu queijo e mandei o para França.
+        Pensei: `ahh, muito difícil, um dos maiores concursos do mundo`. De
+        repente, recebi a notícia que ganhei uma medalha de bronze na França,
+        nossa, quase `morri de gosto`! Depois desse concurso da França, a Débora
+        mesmo trouxe esse concurso da França para o Brasil, para o Museu das
+        Artes em São Paulo, ela fez um Super Concurso Mundial do Queijo Brasil,
+        que é o mesmo concurso da França. Nesta ocasião, mandei 3 queijos e
+        ganhei mais 3 medalhas!
+        <br />
+        <br />
+        No ano passado, teve novamente o concurso da França. Mandei mais 3
+        queijos, e fui premiado novamente! Ganhamos 2 medalhas de prata e 1 de
+        bronze, para nossa completa alegria.
+        <br />
+        <br />E assim caminhamos, trabalhando arduamente para produzir queijos
+        de qualidade e estar entre os melhores queijos do mundo!
+      </p>
+      <br />
+    </div>
+  );
 }
